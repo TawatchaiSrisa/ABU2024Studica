7a60a2fdd768e5d303959f277d9cfd0f97aed482
TawatchaiSrisa <ray39180@gmail.com>
2024-05-27 12:13:00 +0700

    first commit

---
+++ src/main/java/frc/robot/gamepad/GamepadConstants.java
@@ -0,0 +1,45 @@
+package frc.robot.gamepad;
+
+public class GamepadConstants
+{
+    //Controller USB ports
+    public static final int DRIVE_USB_PORT      = 0;
+
+    //PS4
+    //Buttons
+    public static final int A_BUTTON            = 0;
+    public static final int B_BUTTON            = 1;
+    public static final int X_BUTTON            = 2;
+    public static final int Y_BUTTON            = 3;
+    public static final int L1_BUTTON           = 4;
+    public static final int R1_BUTTON           = 5;
+
+    public static final int L2_BUTTON           = 6;
+    public static final int R2_BUTTON           = 7;
+    
+
+    public static final int SQUARE_BUTTON       = 1;
+    public static final int CIRCLE_BUTTON       = 3;
+    public static final int TRIANGLE_BUTTON     = 4;
+    public static final int LEFT_BUMPER         = 5;
+    public static final int RIGHT_BUMPER        = 6;
+    public static final int LEFT_TRIGGER        = 7;
+    public static final int RIGHT_TRIGGER       = 8;
+    public static final int SHARE_BUTTON        = 9;
+    public static final int OPTIONS_BUTTON      = 10;
+    public static final int LEFT_ANALOG_BUTTON  = 11;
+    public static final int RIGHT_ANALOG_BUTTON = 12;
+    public static final int PS4_BUTTON          = 13;
+    public static final int TOUCHPAD_BUTTON     = 14;
+
+    //Joystick
+    public static final int LEFT_ANALOG_X       = 0;
+    public static final int LEFT_ANALOG_Y       = 1;
+    public static final int RIGHT_ANALOG_X      = 2;
+    public static final int RIGHT_ANALOG_Y      = 5;
+
+    //DPAD
+    public static final int DPAD_X              = 4;
+    public static final int DPAD_Y              = 5;
+
+}
